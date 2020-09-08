e2bb1963153fb10edf0328b9b141ff91c7e80c02
Admirated <70275437+Admirated@users.noreply.github.com>
2020-09-08 23:12:52 +0300

    Finish base framework functional

---
+++ src/core/DomListener.js
@@ -0,0 +1,39 @@
+import {capitalize} from '@core/utils'
+
+export class DomListener {
+  constructor($root, listeners = []) {
+    if (!$root) {
+      throw new Error('no $root provided for DomListener!')
+    }
+    this.$root = $root
+    this.listeners = listeners
+  }
+
+  initDOMListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener)
+      console.log(method)
+      if (!this[method]) {
+        const name = this.name || ''
+        throw new Error(
+            `Method ${method}, is not implemented in ${name} Component`
+        )
+      }
+      this[method] = this[method].bind(this)
+      this.$root.on(listener, this[method])
+    })
+  }
+
+  removeDOMListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener)
+      this.$root.off(listener, this[method])
+    })
+  }
+}
+
+function getMethodName(method) {
+  if (typeof method === 'string') {
+    return 'on' + capitalize(method)
+  }
+}
