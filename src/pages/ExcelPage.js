f8174bf22196513efa4a6b8c992e0dc36f6c536b
Admirated <70275437+Admirated@users.noreply.github.com>
2020-10-22 17:13:12 +0300

    Routing implemented

---
+++ src/pages/ExcelPage.js
@@ -0,0 +1,37 @@
+import {Excel} from '@/components/excel/Excel'
+import {Header} from '@/components/header/Header'
+import {Toolbar} from '@/components/toolbar/Toolbar'
+import {Formula} from '@/components/formula/Formula'
+import {Table} from '@/components/table/Table'
+import {createStore} from '@core/createStore'
+import {rootReducer} from '@/redux/rootReducer'
+import {storage, storageName} from '@core/utils'
+import {normalizeInitialState} from '@/redux/initialState'
+import {Page} from '@core/Page'
+
+export class ExcelPage extends Page {
+  getRoot() {
+    const params = this.params ? this.params : Date.now().toString()
+    const state = storage(storageName(params))
+    const store = createStore(rootReducer, normalizeInitialState(state))
+
+    store.subscribe(state => {
+      storage(storageName(params), state)
+    })
+
+    this.excel = new Excel({
+      components: [Header, Toolbar, Formula, Table],
+      store
+    })
+
+    return this.excel.getRoot()
+  }
+
+  afterRender() {
+    this.excel.init()
+  }
+
+  destroy() {
+    this.excel.destroy()
+  }
+}
